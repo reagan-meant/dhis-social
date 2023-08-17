454f4756e47bea7af8639ea9251ef13ea93f49f1
reagan-meant <reaganmeant@gmail.com>
2023-08-17 13:17:35 +0300

    Improve selection of user

--- src/App.js
+++ src/App.js
@@ -23,18 +23,29 @@ const DatasetSelector = () => {
 
   const orgUnitQuery = {
     orgUnit: {
-        resource: 'organisationUnits',
-        params: {
-            fields: ['id,displayName']
-        }
+      resource: 'organisationUnits',
+      params: {
+        fields: ['id,displayName']
+      }
     }
-}
-  const dataValueSetQuery = (datasetId,orgUnit) => ({
+  }
+
+  const userQuery = {
+    user: {
+      resource: 'users',
+      params: {
+        fields: ['telegram'],
+        filter: 'telegram:!null:'
+      }
+    }
+  };
+
+  const dataValueSetQuery = (datasetId, orgUnit) => ({
     dataValueSets: {
       resource: 'dataValueSets.csv',
       params: {
         dataSet: datasetId,
-        orgUnit:orgUnit,
+        orgUnit: orgUnit,
         startDate: '2018-01-01',
         endDate: '2023-10-01',
         paging: false,
@@ -43,14 +54,16 @@ const DatasetSelector = () => {
   });
 
   const { loading: datasetLoading, error: datasetError, data: datasetData } = useDataQuery(datasetQuery);
-  const { loading:loading, error: error, data: orgData } = useDataQuery(orgUnitQuery);
+  const { loading: loading, error: error, data: orgData } = useDataQuery(orgUnitQuery);
+  const { loading: loadingUser, error: userError, data: dhisUsers } = useDataQuery(userQuery);
 
   const { loading: datavalesetLoading, error: datavaluesetError, data: datavluesetData } = useDataQuery(datasetQuery);
   const engine = useDataEngine()
 
   const [selectedDataset, setSelectedDataset] = useState(null);
   const [dataValueSets, setDataValueSets] = useState([]);
-
+  const [dhisUsersValue, setdhisUsersValue] = useState([]);
+  const myarray = [];
 
 
   const [selectedOrganization, setSelectedOrganization] = useState('');
@@ -58,132 +71,134 @@ const DatasetSelector = () => {
 
 
 
-const ben= useDataQuery(orgUnitQuery);
+  const ben = useDataQuery(orgUnitQuery);
 
-console.log("sdfghjjjjjj")
-console.log(orgData)
-console.log(ben)
 
   const handleOrganizationChange = (event) => {
-    
-    console.log("org what")
 
     setSelectedOrganization(event.target.value);
-    console.log("called48884")
-
-    console.log(event.target.value);
   };
 
   useEffect(() => {
-    if (selectedDataset) {
-      console.log("called444")
 
-//      const dataValueSetQueryObj = dataValueSetQuery(selectedDataset);
+    if (dhisUsers) {
+      for (const user of dhisUsers.user.users) {
+        //telegramUsers.push(user.telegram);
+        myarray.push(user.telegram)
 
-    
-      //fetchDataValueSets(dataValueSetQueryObj);
+      }
+      setdhisUsersValue(myarray);
 
+    }
+  }, [selectedDataset, selectedOrganization]);
 
-      if (selectedDataset && selectedOrganization) {
-        //setLoading(true)
-        const dataValueSetQueryObj = dataValueSetQuery(selectedDataset, selectedOrganization);
-
-        engine.query(dataValueSetQueryObj).then((response) => {
-          console.log("benhiiii")
-          console.log(response)
-
-
-
-
-
-
-    // Create a FormData instance to send the Blob
-    const formData = new FormData();
-    formData.append('document', response.dataValueSets, "fileName");
-
+  const fetchUsersWithProfile = async () => {
     try {
-      const chatId = '6051915063' // Replace with the desired chat ID
-      const telegramDocAPIEndpoint =
-      'https://api.telegram.org/bot6393466642:AAGr_SRJA23ev5m_vRWRlp7HjYJqcAZIa_c/sendDocument'
-    
-      //const fileUrl = 'locations.csv'; // Replace with the URL of the file you want to send
-      const caption = 'Caption for the file (optional)';
-
-      const response =  axios.post(telegramDocAPIEndpoint, formData, {
-        
-        params: {chat_id: chatId,
-       // document: formData,
-        caption: caption,
+      const response = await axios.get(`${DHIS2_BASE_URL}/users`, {
+        auth: {
+          username: USERNAME,
+          password: PASSWORD,
         },
-      
-        headers: {
-          'Content-Type': 'multipart/form-data',
+        params: {
+          fields: 'telegram',
+          paging: false,
         },
-      
       });
 
-      console.log('File sent to Telegram:', response.data);
+      // Filter only the users who have a Telegram ID
+      const usersWithTelegram = response.data.users.filter(
+        (user) => user.telegram
+      );
+
+      return usersWithTelegram;
+
     } catch (error) {
-      console.error('Error sending file to Telegram')
-
+      console.error('Error fetching users with profile:', error);
+      throw error;
     }
+  };
 
-
-
-          //const { displayName } = response.dataValueSets.dataValues;
-          
-         // setFormulaDataElementName(displayName)
-      })
-    //  .finally(() => setLoading(false));
-
-
-       /*  engine
-            .query(DATA_ELEMENTS_QUERY, {
-                variables: { id: dataElementId },
-            })
-            .then((response) => {
-                const { displayName } = response.dataElement
-                setFormulaDataElementName(displayName)
-            })
-            .finally(() => setLoading(false)) */
-    }
-
-   
-    }
-  }, [selectedDataset,selectedOrganization]);
 
   const handleDatasetChange = (event) => {
     const datasetId = event.target.value;
     console.log(datasetId)
     setSelectedDataset(datasetId);
 
-   /*  const dataValueSetQueryObj = dataValueSetQuery(datasetId);
 
 
-    const { loading, error, data } = useDataQuery(dataValueSetQueryObj);
-    if (!loading && !error && data && data.dataValueSets) {
-      setDataValueSets(data.dataValueSets);
-    } */
   };
-console.log(datasetData ?datasetData.datasets.dataSets:'jjj');
+
+  const sendFileToTelegram = () => {
+
+    if (selectedDataset) {
+      if (selectedDataset && selectedOrganization && dhisUsersValue) {
+        //setLoading(true)
+
+        const dataValueSetQueryObj = dataValueSetQuery(selectedDataset, selectedOrganization);
+
+        engine.query(dataValueSetQueryObj).then((response) => {
+
+          // Create a FormData instance to send the Blob
+          const formData = new FormData();
+          formData.append('document', response.dataValueSets, "fileName");
 
 
-const fetchDataValueSets = async (queryObj) => {
-  console.log(
-    "dddd"
-  )
-  const { loading, error, data } = useDataQuery(queryObj);
+          for (const dhis of dhisUsersValue) {
 
-  try {
-    const { loading, error, data } = useDataQuery(queryObj);
-console.log(data)
-    if (!loading && !error && data && data.dataValueSets) {
-      setDataValueSets(data.dataValueSets);
+            try {
+              //const chatId = '6051915063' // Replace with the desired chat ID
+              const chatId = dhis // Replace with the desired chat ID
+
+              const telegramDocAPIEndpoint =
+                'https://api.telegram.org/bot6393466642:AAGr_SRJA23ev5m_vRWRlp7HjYJqcAZIa_c/sendDocument'
+
+              //const fileUrl = 'locations.csv'; // Replace with the URL of the file you want to send
+              const caption = 'Caption for the file (optional)';
+
+              const response = axios.post(telegramDocAPIEndpoint, formData, {
+
+                params: {
+                  chat_id: chatId,
+                  // document: formData,
+                  caption: caption,
+                },
+
+                headers: {
+                  'Content-Type': 'multipart/form-data',
+                },
+
+              });
+
+              console.log('File sent to Telegram:', response.data);
+            } catch (error) {
+              console.error('Error sending file to Telegram')
+
+            }
+          }
+
+        })
+      }
+
+
     }
-  } catch (error) {
-    console.error('Error fetching data value sets:', error);
   }
-};
+
+  const fetchDataValueSets = async (queryObj) => {
+    console.log(
+      "dddd"
+    )
+    const { loading, error, data } = useDataQuery(queryObj);
+
+    try {
+      const { loading, error, data } = useDataQuery(queryObj);
+      console.log(data)
+      if (!loading && !error && data && data.dataValueSets) {
+        setDataValueSets(data.dataValueSets);
+      }
+    } catch (error) {
+      console.error('Error fetching data value sets:', error);
+    }
+  };
   return (
     <div>
       <h3>Select a Dataset</h3>
@@ -205,22 +220,25 @@ console.log(data)
         <span>No datasets available.</span>
       )}
 
-<h3>Select an org</h3>
-{orgData && orgData.orgUnit.organisationUnits ? (
+      <h3>Select an org</h3>
+      {orgData && orgData.orgUnit.organisationUnits ? (
 
-<div>
-      <select value={selectedOrganization} onChange={handleOrganizationChange}>
-        <option value="">Select an organization</option>
-        {orgData.orgUnit.organisationUnits.map((organization) => (
-          <option key={organization.id} value={organization.id}>
-            {organization.displayName}
-          </option>
-        ))}
-      </select>
-    </div>):     (   <span>No orgs available.</span>)}
+        <div>
+          <select value={selectedOrganization} onChange={handleOrganizationChange}>
+            <option value="">Select an organization</option>
+            {orgData.orgUnit.organisationUnits.map((organization) => (
+              <option key={organization.id} value={organization.id}>
+                {organization.displayName}
+              </option>
+            ))}
+          </select>
+        </div>) : (<span>No orgs available.</span>)}
 
-
+      <button onClick={() => sendFileToTelegram()}>
+        Send Telegram Message
+      </button>
     </div>
+
   );
 };
 
